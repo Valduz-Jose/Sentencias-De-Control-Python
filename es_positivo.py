8d4a2b6270ef3bd14ef7078d31f2d6b5c7d71d6d
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-16 11:20:55 -0400

    Agregar archivos para la evaluación de números positivos y sentencias de decisión

---
+++ es_positivo.py
@@ -0,0 +1,10 @@
+print("***Revision Valor Positivo****")
+
+numero = int(input("Ingrese un numero: "))
+
+if numero > 0:
+  print(f"El numero {numero} es positivo")
+elif numero == 0:
+  print(f"El numero {numero} es cero")
+else:
+  print(f"El numero {numero} es negativo")
---
+++ sentencias_decision.py
@@ -0,0 +1,14 @@
+# Sentencias de decision if else y elif
+
+# if condition:
+#    # code to execute if condition is true
+# else:
+#    # code to execute if condition is false
+edad = 30
+if edad>=18:
+  print(f"Eres mayor de edad tienes {edad} a*os")
+elif 13<= edad < 18:
+  print(f"Eres un adolescente tienes {edad} a*os")
+else:
+  print(f"No eres mayor de edad tienes {edad} a*os")
+
